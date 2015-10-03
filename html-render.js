16bd089bc90ec2e5047da3c5e94e6f15c96e488c
Mathew Weitzel <mweitzel@gmail.com>
2015-10-03 19:03:30 -0400

    add module that renders specified jsx file

---
+++ html-render.js
@@ -0,0 +1,6 @@
+var React = require('react')
+
+module.exports = function(jsxTemplate, properties) {
+  var factory = React.createFactory(require(jsxTemplate))
+  return React.renderToStaticMarkup(factory(properties))
+}
