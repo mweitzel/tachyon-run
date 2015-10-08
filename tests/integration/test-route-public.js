e998b561c5ce33127d10e59217e5dc7ad4533a32
Mathew Weitzel <mweitzel@gmail.com>
2015-10-08 10:16:03 -0400

    clarify/clean up test for route

--- tests/integration/test-route-public.js
+++ tests/integration/test-route-public.js
@@ -5,20 +5,15 @@ var test = require('tape')
   , compose = require('koa-compose')
 
 test('public route passes to next when not found', function (t) {
-  t.plan(1)
+  t.plan(2)
   var ctx = {}
 
   co.wrap(compose([
     function *(next) { yield* pub.call(this, 'sindafs.ps', next) }
-  , function *(next) {
-      t.equal(this.body, undefined)
-      yield next
-    }
+  , function *(next) { t.pass() }
   ])).call(ctx)
   .then(
-    function() {}
-  , function(err) {
-      t.end(err)
-    }
+    t.pass.bind(t)
+  , t.fail.bind(t)
   )
 })
