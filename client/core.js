2579ce3aeb46ead3c9db710b93e7babb04164224
Mathew Weitzel <mweitzel@gmail.com>
2015-10-12 12:00:59 -0400

    fix styling consistency

--- client/core.js
+++ client/core.js
@@ -10,32 +10,32 @@ function Core(window, context) {
 }
 
 Core.prototype = {
-  physicsTimeStep: 1000/60,
-  entities:[],
-  draw: function() {
+  physicsTimeStep: 1000/60
+, entities: []
+, draw: function() {
     this.context.clearRect(0, 0, 640, 480)
 
     for (var i=0; i < this.entities.length; i++) {
       this.entities[i].draw(this.context)
     }
-  },
-  step: function() {
+  }
+, step: function() {
     for (var i=0; i < this.entities.length; i++) {
       this.entities[i].update()
     }
     this.lastUpdate += this.physicsTimeStep
-  },
-  lastUniqueId: function(){
+  }
+, lastUniqueId: function(){
     return 0
-  },
-  uniqueId: function(){
+  }
+, uniqueId: function(){
     var a = this.lastUniqueId()
     this.lastUniqueId = function(){
       return a+1
     }
     return a + 1
-  },
-  update: function() {
+  }
+, update: function() {
     var currentTime = new Date().getTime()
 
     // if frames back up 0.5 seconds, eat the lag and move on
@@ -49,18 +49,18 @@ Core.prototype = {
     }
 
     this.draw()
-  },
-  updateAndRegisterNextUpdate: function() {
+  }
+, updateAndRegisterNextUpdate: function() {
     if(this.paused) { return }
     this.update()
     this.window.requestAnimationFrame(this.updateAndRegisterNextUpdate.bind(this))
   }
-, paused: true,
-  pause: function(){ this.paused = true },
-  play: function(){ this.paused = false ; this.updateAndRegisterNextUpdate() }
+, paused: true
+, pause: function(){ this.paused = true }
+, play: function(){ this.paused = false ; this.updateAndRegisterNextUpdate() }
 , start: function(){ return this.play() }
 , stop: function(){ return this.pause() }
-, togglePaused: function(){ this.paused = !this.paused },
+, togglePaused: function(){ this.paused = !this.paused }
 }
 
 function Input(core) {
@@ -80,28 +80,28 @@ function Input(core) {
 }
 
 Input.prototype = {
-  keyCodesDown:[],
-  keyCodesUp:[],
-  downAt:function(keyCode){
+  keyCodesDown: []
+, keyCodesUp: []
+, downAt: function(keyCode){
     return this.keyCodesDown[keyCode] || 0
-  },
-  upAt:function(keyCode){
+  }
+, upAt: function(keyCode){
     return this.keyCodesUp[keyCode] || 0
-  },
-  getKey:function(keyCode){
+  }
+, getKey: function(keyCode){
     // frames are not backed up
     // both keydown and keyup in same frame when backed up
     return ((this.downAt(keyCode) > this.upAt(keyCode)) && this.downAt(keyCode) < this.core.lastUpdate + this.core.physicsTimeStep) || this.getKeyDown(keyCode)
-  },
-  getKeyDown:function(keyCode){
+  }
+, getKeyDown: function(keyCode){
     //frames are not backed up
     return  (this.downAt(keyCode) > this.core.lastUpdate && this.downAt(keyCode) < this.core.lastUpdate + this.core.physicsTimeStep)
-  },
-  getKeyUp:function(keyCode){
+  }
+, getKeyUp: function(keyCode){
     //frames are not backed up
     return (this.upAt(keyCode) > this.core.lastUpdate && this.upAt(keyCode) < this.core.lastUpdate + this.core.physicsTimeStep)
-  },
-  downDuration:function(keyCode){
+  }
+, downDuration: function(keyCode){
     return Math.max(0, this.core.lastUpdate - this.downAt(keyCode))
   }
 
