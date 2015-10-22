a5378c1a075b6ba0253321a0308cffcae6b8693d
Mathew Weitzel <mweitzel@gmail.com>
2015-10-22 14:52:43 -0400

    oops, never commit these files.. configuration mosly

---
+++ client/keys.js
@@ -0,0 +1,10 @@
+module.exports = {
+  ENTER: 13
+, SPACE: 32
+, ZERO: 48
+, ONE: 49
+, TWO: 50
+, THREE: 51
+, FOUR: 52
+, FIVE: 53
+}
---
+++ client/media-loader.js
@@ -0,0 +1,14 @@
+var fs = require('fs')
+
+module.exports = {
+  sounds: {
+    thunk: [ 'mp3', fs.readFileSync('test/media/thunk.mp3', 'base64') ]
+  }
+, images: {
+    atlas: [ 'png', fs.readFileSync('media/atlas-1.png', 'base64') ]
+  }
+, text: {
+    atlas: [ 'json', fs.readFileSync('media/atlas-1.json', 'ascii') ]
+  , sprite: [ 'json', fs.readFileSync('media/sprite-meta.json', 'ascii') ]
+  }
+}
