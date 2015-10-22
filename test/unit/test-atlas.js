98fa590b2f6513bb976e047c2858d4d68e7ec8eb
Mathew Weitzel <mweitzel@gmail.com>
2015-10-22 14:48:46 -0400

    fix test for atlas requiring global

--- test/unit/test-atlas.js
+++ test/unit/test-atlas.js
@@ -2,12 +2,10 @@ var test = require('tape')
 
 test('depends on global Image function', function(t) {
   t.plan(2)
-  t.throws(function() {
-    require('../../client/atlas')
-  })
   t.doesNotThrow(function() {
     Image = function() {}
-    require('../../client/atlas')
+    var client = require('../../client/atlas')
     delete Image
+    t.ok(client.image.isTestHelper)
   })
 })
