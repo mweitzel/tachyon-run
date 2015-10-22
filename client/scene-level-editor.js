b8b376a4e9d22e0cf4c9dd0e3e1c16af3024233c
Mathew Weitzel <mweitzel@gmail.com>
2015-10-22 14:54:41 -0400

    add level editor as a scene

---
+++ client/scene-level-editor.js
@@ -0,0 +1,16 @@
+var format = require('../format-base64-for-html')
+var keys = require('./keys')
+  , atlas = require('./atlas')
+  , spriteMeta = require('./sprite-meta-data')
+  , Sprite = require('./sprite').bind(null, atlas, spriteMeta)
+  , drawSprite = require('./sprite').draw
+  , _ = require('lodash')
+
+
+module.exports = function(core) {
+  
+}
+
+function Cursor() {
+  
+}
