c0a548d03b4cc05afcf4028b21dab4c66066a92b
Mathew Weitzel <mweitzel@gmail.com>
2015-10-23 15:12:09 -0400

    piece previewer is added to level editor scene

--- client/scene-level-editor.js
+++ client/scene-level-editor.js
@@ -1,7 +1,10 @@
-var format = require('../format-base64-for-html')
 var keys = require('./keys')
   , Sprite = require('./sprite-preconfigured')
+  , getSpriteNames = require('./sprite-name-prefixes')
+  , atlasIndex = require('./sprite-atlas-index.js')
+  , spriteNames = getSpriteNames(Object.keys(atlasIndex.frames))
   , Cursor = require('./level-editor-cursor')
+  , Preview = require('./level-editor-piece-previewer')
 
 
 module.exports = function(core) {
@@ -9,6 +12,11 @@ module.exports = function(core) {
 
   // temporary, for visual reference
   core.entities.push(new BlockA())
+
+  var preview = new Preview(makeSprites(spriteNames), core)
+  var cameraSize = core.cameraSize
+  preview.follow(core.cameraCenter, -cameraSize.x/2, -cameraSize.y/2)
+  core.entities.push(preview)
 }
 
 function BlockA() { }
@@ -17,4 +25,10 @@ BlockA.prototype = {
 , y: 0
 , sprite: new Sprite('block_a')
 , draw: Sprite.draw
+}
+
+function makeSprites() {
+  return spriteNames.map(function(name) {
+    return new Sprite(name)
+  })
 }
--- test/unit/test-scene-level-editor.js
+++ test/unit/test-scene-level-editor.js
@@ -1,15 +1,27 @@
 var test = require('tape')
   , Editor = require('../../client/scene-level-editor')
   , Cursor = require('../../client/level-editor-cursor')
+  , Previewer = require('../../client/level-editor-piece-previewer')
   , td = require('testdouble')
 
 test('adds cursor to the scene', function(t) {
   t.plan(1)
 
-  var core = { entities: { push: td.create() } }
+  var core = { entities: { push: td.create() }, cameraSize: { x:10, y:10 } }
   var editor = new Editor(core)
 
   t.doesNotThrow(function() {
     td.verify(core.entities.push(td.matchers.isA(Cursor)))
+  })
+})
+
+test('adds piece previewer to the scene', function(t) {
+  t.plan(1)
+
+  var core = { entities: { push: td.create() }, cameraSize: { x:10, y:10 } }
+  var editor = new Editor(core)
+
+  t.doesNotThrow(function() {
+    td.verify(core.entities.push(td.matchers.isA(Previewer)))
   })
 })
