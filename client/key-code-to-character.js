83742cd5a9529a5550a4c4b3ecc39e82133e99ae
Mathew Weitzel <mweitzel@gmail.com>
2015-10-29 10:53:00 -0400

    module to turn keycodes back into characters

---
+++ client/key-code-to-character.js
@@ -0,0 +1,27 @@
+var _ = require('lodash')
+  , keys = require('./keys')
+
+module.exports = function(capitalize, keyCode) {
+  var char = _.findKey(keys , function(value) { return value == keyCode } )
+  char = char === 'SPACE' ? ' ' : char
+  if(typeof char !== 'string' || ( char.length !== 1 )) { return }
+  return capitalize ? keyShift(char) : char.toLowerCase()
+}
+
+var capitalizedKeyMap = {
+    1: '!'
+  , 2: '@'
+  , 3: '#'
+  , 4: '$'
+  , 5: '%'
+  , 6: '^'
+  , 7: '&'
+  , 8: '*'
+  , 9: '('
+  , 0: ')'
+}
+
+function keyShift(char) {
+  console.log(char)
+  return capitalizedKeyMap[char] || char.toUpperCase()
+}
