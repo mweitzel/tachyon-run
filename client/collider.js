5ca80ebacfee6eccad6a65709be20b9a8a95bfdd
Mathew Weitzel <mweitzel@gmail.com>
2015-11-01 14:52:14 -0500

    add funcitons to collider to determine collision response

--- client/collider.js
+++ client/collider.js
@@ -55,20 +55,97 @@ enemies
 
 
 module.exports = {
-  collidesWith: function(other) {
-    return intersect(this.bounds, other.bounds)
-  }
-, intersects: function(b1, b2) {
-    return ((
-        ( b1[0] <= b2[0] + b2[2] && b1[0] >= b2[0] )                 // my left  is between their right and left
-     || ( b1[0] + b1[2] <= b2[0] + b2[2] && b1[0] + b1[2] >= b2[0] ) // my right is between their right and left
-     || ( b2[0] <= b1[0] + b1[2] && b2[0] >= b1[0] )                 // their left  is between my right and left
-     || ( b2[0] + b2[2] <= b1[0] + b1[2] && b2[0] + b2[2] >= b1[0] ) // their right is between my right and left
-    ) && (
-        ( b1[1] <= b2[1] + b2[3] && b1[1] >= b2[1] )                 // my bottom  is between their top and bottom
-     || ( b1[1] + b1[3] <= b2[1] + b2[3] && b1[1] + b1[3] >= b2[1] ) // my top     is between their top and bottom
-     || ( b2[1] <= b1[1] + b1[3] && b2[1] >= b1[1] )                 // their bottom is between my top and bottom
-     || ( b2[1] + b2[3] <= b1[1] + b1[3] && b2[1] + b2[3] >= b1[1] ) // their top    is between my top and bottom
-    ))
-  }
+  playerShouldRespondTo:playerShouldRespondTo
+, collidesWith: collidesWith
+, intersects: intersects
+, collidingSide: collidingSide
+}
+
+function playerShouldRespondTo(player, other) {
+  return other.collidable
+   && ( (player.hasMoved && other.layer == 'ground')
+      || other.team === 'enemy'
+      || other.isTrigger
+      )
+   && collidesWith.call(player, other)
+}
+
+function collidesWith(other) {
+  return intersects(this.bounds(), other.bounds())
+}
+
+function collidingSide(subject, collider) {
+  if(yOverlaps(subject, collider))
+    return leftIsCloserThanRight(subject, collider) ? 'left' : 'right'
+  if(xOverlaps(subject, collider))
+    return topIsCloserThanBottom(subject, collider) ? 'top' : 'bottom'
+
+  return closestSide(subject, collider)
+}
+
+function closestSide(subject, collider) {
+  var distances = [
+    topAbsDist
+  , bottomAbsDist
+  , leftAbsDist
+  , rightAbsDist
+  ].map(function(f) { return f(subject, collider) })
+  return [
+    'top'
+  , 'bottom'
+  , 'left'
+  , 'right'
+  ][distances.indexOf(Math.min.apply(Math, distances))]
+}
+
+function leftIsCloserThanRight(subject, collider) {
+  return leftAbsDist(subject, collider) < rightAbsDist(subject, collider)
+}
+
+function topIsCloserThanBottom(subject, collider) {
+  return topAbsDist(subject, collider) < bottomAbsDist(subject, collider)
+}
+
+function topAbsDist(subject, collider) {
+  return absoluteDistance(subject[1], collider[1] + collider[3])
+}
+
+function bottomAbsDist(subject, collider) {
+  return absoluteDistance(subject[1] + subject[3], collider[1])
+}
+
+function leftAbsDist(subject, collider) {
+  return absoluteDistance(subject[0], collider[0] + collider[2])
+}
+
+function rightAbsDist(subject, collider) {
+  return absoluteDistance(subject[0] + subject[2], collider[0])
+}
+
+
+
+function absoluteDistance(a, b) {
+  return Math.abs(a - b)
+}
+
+function intersects(b1, b2) {
+  return xOverlaps(b1, b2) && yOverlaps(b1, b2)
+}
+
+function xOverlaps(b1, b2) {
+  return (
+      ( b1[0] <= b2[0] + b2[2] && b1[0] >= b2[0] )                 // my left  is between their right and left
+   || ( b1[0] + b1[2] <= b2[0] + b2[2] && b1[0] + b1[2] >= b2[0] ) // my right is between their right and left
+   || ( b2[0] <= b1[0] + b1[2] && b2[0] >= b1[0] )                 // their left  is between my right and left
+   || ( b2[0] + b2[2] <= b1[0] + b1[2] && b2[0] + b2[2] >= b1[0] ) // their right is between my right and left
+  )
+}
+
+function yOverlaps(b1, b2) {
+  return (
+      ( b1[1] <= b2[1] + b2[3] && b1[1] >= b2[1] )                 // my bottom  is between their top and bottom
+   || ( b1[1] + b1[3] <= b2[1] + b2[3] && b1[1] + b1[3] >= b2[1] ) // my top     is between their top and bottom
+   || ( b2[1] <= b1[1] + b1[3] && b2[1] >= b1[1] )                 // their bottom is between my top and bottom
+   || ( b2[1] + b2[3] <= b1[1] + b1[3] && b2[1] + b2[3] >= b1[1] ) // their top    is between my top and bottom
+  )
 }
--- test/unit/test-collider.js
+++ test/unit/test-collider.js
@@ -21,10 +21,10 @@ test('#intersects rejects non overlapping boxes', function(t) {
 /*
   ---
  |   |
- |   | ----- 
+ |   | -----
   --- |     |
       |     |
-       ----- 
+       -----
 */
   t.plan(1)
   var b1 = [10,10,5,5]
@@ -47,4 +47,96 @@ test('#intersects finds oddly boxes', function(t) {
   var b1 = [10,10,2,10]
     , b2 = [8, 12, 10, 2]
   t.true(collider.intersects(b1, b2))
+})
+
+test('collidingSide returns left when left is closest', function(t) {
+/*
+  -------
+ |       |
+ |       | ---------
+  ------- | subject |
+           ---------
+*/
+  t.plan(1)
+  var subjectBounds = [10,10,10,10]
+  var colliderBounds = [0,0,10,11]
+  t.equal(
+    collider.collidingSide(subjectBounds, colliderBounds)
+  , 'left'
+  )
+})
+
+test('collidingSide returns right when right is closest', function(t) {
+/*
+              ------
+             |      |
+  ---------  |      |
+ | subject |  ------
+  ---------
+*/
+  t.plan(1)
+  var subjectBounds =  [ 0,10,10,10]
+  var colliderBounds = [11, 0,11,12]
+  t.equal(
+    collider.collidingSide(subjectBounds, colliderBounds)
+  , 'right'
+  )
+})
+
+test('collidingSide returns top when top is closest', function(t) {
+/*
+
+ ------
+|      |
+|      |
+ ------
+  ---------
+ | subject |
+  ---------
+*/
+  t.plan(1)
+  var subjectBounds =  [ 1,11,10,10]
+  var colliderBounds = [ 0, 0,10,10]
+  t.equal(
+    collider.collidingSide(subjectBounds, colliderBounds)
+  , 'top'
+  )
+})
+
+test('collidingSide returns bottom when bottom is closest', function(t) {
+/*
+  ---------
+ | subject |
+  ---------
+ ------
+|      |
+|      |
+ ------
+*/
+  t.plan(1)
+  var subjectBounds =  [ 1, 0,10,10]
+  var colliderBounds = [ 0,11,10,10]
+  t.equal(
+    collider.collidingSide(subjectBounds, colliderBounds)
+  , 'bottom'
+  )
+})
+
+test('collidingSide when no axis overlap, returns closest', function(t) {
+/*
+ ---------
+| subject |
+ ---------
+                      ------
+                     |      |
+                     |      |
+                      ------
+*/
+  t.plan(1)
+  var subjectBounds =  [  0, 0,10,10]
+  var colliderBounds = [ 20,11,10,10]
+  t.equal(
+    collider.collidingSide(subjectBounds, colliderBounds)
+  , 'bottom'
+  )
 })
