ea7d5e2d63d719400ae363b18eef061b38830144
Mathew Weitzel <mweitzel@gmail.com>
2015-11-08 15:15:21 -0500

    use actual image

--- test/unit/test-background.js
+++ test/unit/test-background.js
@@ -5,7 +5,7 @@ test('appropriate #draw is used for different types of strings', function(t) {
   t.plan(2)
   var b = new Background('#303')
   t.equal(b.draw, Background.drawColor)
-  var b = new Background('background_blue_rocky')
+  var b = new Background('background_a')
   t.equal(b.draw, Background.drawTiledImage)
 })
 
