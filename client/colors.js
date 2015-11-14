2426bf8b19e08111c2958b15039448c362315da0
Mathew Weitzel <mweitzel@gmail.com>
2015-11-14 12:38:30 -0500

    add colors

--- client/colors.js
+++ client/colors.js
@@ -1,4 +1,8 @@
 module.exports = {
   debugCollider: { collidable: '#D33', nonCollidable: '#33D', special: '#3D3' }
-, textBackground: '#EFF'
+, textBackground: '#152a3e'
+, menuModelBackgroundFade: 'rgba(0,0,0,0.5)'
+, menuBackground: '#152a3e'
+, menuBorder: '#000'
+, transparent: 'rgba(0,0,0,0)'
 }
