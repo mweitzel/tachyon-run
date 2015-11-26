32517c8dba4a276aa11ef7d2971e93fd3c101026
Mathew Weitzel <mweitzel@gmail.com>
2015-11-26 11:12:40 -0500

    != to !== in collider comparison

--- client/collider.js
+++ client/collider.js
@@ -71,7 +71,7 @@ function defaultShouldRespond(other) {
   return this.collidable
   && other.collidable
   && ( other.layer == 'ground'
-    || ( this.team != other.team )
+    || ( this.team !== other.team )
     )
   && collidesWith.call(this, other)
 }
