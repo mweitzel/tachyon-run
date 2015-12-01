f6c1b68c2c71778891fb4739fbef2493e0ffd306
Mathew Weitzel <mweitzel@gmail.com>
2015-12-01 12:29:02 -0500

    update player controls, can "use" closest object if usable

--- client/player.js
+++ client/player.js
@@ -2,6 +2,7 @@ var _ = require('lodash')
   , Sprite = require('./sprite-preconfigured')
   , keys = require('./keys')
   , sprites = require('./all-sprites')
+  , closestTo = require('./collider').objClosestTo
 
 module.exports = Player
 
@@ -21,24 +22,77 @@ Player.prototype = _.merge(
     bounds: function() {
       return [this.x-7, this.y-24, 14, 24]
     }
-  , draw: Sprite.draw
+  , draw: function(ctx) {
+      Sprite.draw.call(this, ctx)
+    }
+  , pickSprite: function(core) {
+      var getKey = core.input.getKey.bind(core.input)
+      var mirroredLastTime = this.sprite.mirror
+
+      if(this.__lastGroundCollisionSides.indexOf('bottom') > -1)
+        this.sprite = sprites.get('char_indoor_stand')
+      else
+        this.sprite = sprites.get('char_indoor_stand')
+        //this.sprite = sprites.get('char_c')
+
+      this.sprite.mirror = (
+        ( getKey(keys.RIGHT) && getKey(keys.LEFT) && this.dx > 0 )
+      ||( !getKey(keys.RIGHT) && !getKey(keys.LEFT) && this.dx > 0 )
+      ||( getKey(keys.RIGHT) && !getKey(keys.LEFT))
+      ||( Math.abs(this.dx) < 0.01 && mirroredLastTime )
+      )
+    }
+  , postPhysicsAndDamageHandler: function(core, stillCollidesWithMe) {
+      var usable = _.filter(stillCollidesWithMe, function(obj) { return !!obj.use })
+      if(!_.isEmpty(usable) && core.input.getKeyDown(keys.E)){
+        closestTo.call(this, usable).use(this, core)
+      }
+      //if not moving, show ? or ... to show inspectabll
+    }
   , respondToControllerIntent: function(core) {
-      if(core.input.getKey(keys.RIGHT)) {
-        this.sprite.mirror = true
-        this.dx += 0.01
+      if(this.__lastGroundCollisionSides.indexOf('bottom') > -1) {
+        if(core.input.getKey(keys.RIGHT)) {
+          this.sprite.mirror = true
+          this.dx += 0.02
+        }
+        else if(core.input.getKey(keys.LEFT)) {
+          this.sprite.mirror = false
+          this.dx -= 0.02
+        }
+        else { this.dx *= 0.9
+          if(Math.abs(this.dx) < 0.01) { this.dx = 0 }
+        }
       }
-      else if(core.input.getKey(keys.LEFT)) {
-        this.sprite.mirror = false
-        this.dx -= 0.01
-      }
-      else { this.dx *= 0.9
-        if(Math.abs(this.dx) < 0.01) { this.dx = 0 }
+      else {
+        if(core.input.getKey(keys.RIGHT)) {
+          this.sprite.mirror = true
+          this.dx += 0.005
+        }
+        else if(core.input.getKey(keys.LEFT)) {
+          this.sprite.mirror = false
+          this.dx -= 0.005
+        }
+        else {
+          this.dx *= 0.99
+          if(Math.abs(this.dx) < 0.01) { this.dx = 0 }
+        }
       }
 
       if(core.input.getKeyDown(keys.SPACE)) {
         if(this.__lastGroundCollisionSides.indexOf('bottom') > -1) {
-          this.dy = -0.25
+          this.dy = -0.15
         }
+      }
+      if(core.input.getKey(keys.SPACE)) {
+        this.dy -= 0.005
+        this.maxDy = 0.1
+      }
+      else {
+        this.maxDy = 0.2
+      }
+
+      if(core.input.getKeyDown(keys.F)) {
+        this.currentWeapon && this.currentWeapon.fire(core)
       }
     }
   }
