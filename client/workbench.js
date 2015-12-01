1f24bd4cb2a87672c250cd86005b63602a22f4d7
Mathew Weitzel <mweitzel@gmail.com>
2015-12-01 08:44:29 -0500

    workbench can be used to manage materias across weapons

--- client/workbench.js
+++ client/workbench.js
@@ -10,6 +10,7 @@ var allSprites = require('./all-sprites')
   , Round = require('../round')
   , Rstring = require('./renderable-string')
   , _ = require('lodash')
+  , materiaData = require('./materias')
 
 module.exports = Workbench
 
@@ -18,17 +19,22 @@ function Workbench(core, player) {
   this.exit = core.removePriorityObj.bind(core, this)
 
   this.player = player
+  this.renderCurrentWeaponMateria = new Round()
   this.materias = new Round(player.computeUnusedMaterias())
-
-  this.setActiveWeapon()
-  this.rstring = new Rstring('')
-  this.rstring.backgroundColor = colors.transparent
-
   this.panels = new Round([
     this.renderCurrentWeaponMateria
   , this.materias
   ])
-  this.panels.current.active = true
+
+  this.editableItems = new Round(Object.keys(this.player.weapons))
+
+  this.setActiveWeapon()
+
+  this.materiaPreview = new Rstring('')
+  this.materiaPreview.backgroundColor = colors.transparent
+
+  this.currentItemSlotsFilledText = new Rstring('')
+  this.currentItemSlotsFilledText.backgroundColor = colors.transparent
 }
 
 Workbench.prototype = {
@@ -37,43 +43,60 @@ Workbench.prototype = {
     ctx.fillRect.apply(ctx, [0,0,ctx.width,ctx.height])
     ctx.fillRect.apply(ctx, paddedFullRegion(ctx))
     ctx.fillRect.apply(ctx, paddedFullRegion(ctx))
-    menuBorder(paddedFullRegion(ctx))
+    var pfRegion = paddedFullRegion(ctx)
+    menuBorder(pfRegion)
     .draw(ctx)
+    drawHBar(ctx)
     arrangeActiveWeapon(xywhObjHoldingSprite(this.activeSprite), ctx)
     .draw(ctx)
-    arrangeMateriaPreviewText(this.rstring, ctx)
+    arrangeMateriaPreviewText(this.materiaPreview, ctx)
+    .draw(ctx)
+    arrangeCurrentItemSlotsFilledText(this.currentItemSlotsFilledText, ctx)
     .draw(ctx)
 
-    this.drawMateriaList(this.materias, [0.5,0,0.5,1], this.isActivePanel(this.materias), ctx)
-    this.drawMateriaList(this.renderCurrentWeaponMateria, [0,0,0.5,1], this.isActivePanel(this.renderCurrentWeaponMateria), ctx)
+    this.drawMateriaList(this.materias, availableMateriaRegion(ctx), this.isActivePanel(this.materias), true, 5, ctx)
+    this.drawMateriaList(this.renderCurrentWeaponMateria, currentWeaponMateriaRegion(ctx), this.isActivePanel(this.renderCurrentWeaponMateria), false, 5, ctx)
   }
 , isActivePanel: function(supposedlyPanel) {
     return this.panels.current === supposedlyPanel
   }
-, drawMateriaList: function(materiaList, subRegionBounds, showSelected, ctx) {
-    var materiaDrawRegion = padMenuBorder(subRegion(paddedFullRegion(ctx), subRegionBounds))
-    for(var i = 0; i < materiaList.array.length; i++) {
+, drawMateriaList: function(materiaList, subRegionBounds, showSelected, displayPreviewTextToRight, maxDisplay, ctx) {
+    var maxDisplay = maxDisplay || 5
+    var tempText = new Rstring('')
+    var selected = materiaList.current
+    tempText.backgroundColor = colors.transparent
+    var beginIndex = Math.floor(materiaList.index / maxDisplay) * maxDisplay
+    var maxIndex = beginIndex + maxDisplay
+
+    for(var i = beginIndex; i < maxIndex && i < materiaList.array.length; i++) {
+      var yOffset = (i-beginIndex)*config.tileSize
       var materia = materiaList.array[i]
       draw.call(
         { sprite: allSprites.get(materia.split('_')[0])
-        , x: materiaDrawRegion[0]
-        , y: materiaDrawRegion[1] + i*config.tileSize
+        , x: subRegionBounds[0]
+        , y: subRegionBounds[1] + yOffset
         }
       , ctx)
-      if(showSelected && i === materiaList.index ) {
+      if(showSelected && materia === selected ) {
         draw.call(
           { sprite: allSprites.get('selected')
-          , x: materiaDrawRegion[0]
-          , y: materiaDrawRegion[1] + i*config.tileSize
+          , x: subRegionBounds[0]
+          , y: subRegionBounds[1] + yOffset
           }
         , ctx)
+      }
+      if(displayPreviewTextToRight) {
+        tempText.string = generateMateriaListPreiewText(materia)
+        tempText.x = subRegionBounds[0] + 1.5 * config.tileSize
+        tempText.y = subRegionBounds[1] + yOffset
+        tempText.draw(ctx)
       }
     }
     if(showSelected && materiaList.array.length === 0 ) {
         draw.call(
           { sprite: allSprites.get('selected')
-          , x: materiaDrawRegion[0]
-          , y: materiaDrawRegion[1] + i*config.tileSize
+          , x: subRegionBounds[0]
+          , y: subRegionBounds[1] + i*config.tileSize
           }
         , ctx)
     }
@@ -97,36 +120,52 @@ Workbench.prototype = {
     if(core.input.getKeyDown(keys.LEFT)){
       this.panels.previous()
     }
+    if(core.input.getKeyDown(keys['['])) {
+      this.editableItems.previous()
+      this.setActiveWeapon()
+    }
+    if(core.input.getKeyDown(keys[']'])) {
+      this.editableItems.next()
+      this.setActiveWeapon()
+    }
     if(core.input.getKeyDown(keys.SPACE)){
       if(this.panels.current === this.materias)
         this.tryToPutMateriaOnItem()
       else
         this.tryToRemoveMateriaFromItem()
     }
-    this.rstring.string = this.panels.current.current || '--'
+    this.materiaPreview.string = this.panels.current.current
+    ? generateWorkbenchSynopsis(this.panels.current.current)
+    : '--'
+    this.currentItemSlotsFilledText.string = [
+      this.renderCurrentWeaponMateria.array.length
+    , this.getPlayersItemFromCurrentEditable().materiaArity
+    ].join('/')
   }
-, tryToPutMateriaOnItem() {
+, tryToPutMateriaOnItem: function() {
     if(!this.currentWeaponHasOpenMateriaSlots()) { return }
     if(!this.materias.current) { return }
-    this.player.weapons[this.currentWeapon].materia.push(this.materias.current)
-    this.renderCurrentWeaponMateria.array = this.player.weapons[this.currentWeapon].materia
+    this.player.weapons[this.editableItems.current].materia.push(this.materias.current)
+    this.renderCurrentWeaponMateria.array = this.player.weapons[this.editableItems.current].materia
     this.materias.array = this.player.computeUnusedMaterias()
   }
-, currentWeaponHasOpenMateriaSlots() {
-    var actualWeapon = this.player.weapons[this.currentWeapon]
+, currentWeaponHasOpenMateriaSlots: function() {
+    var actualWeapon = this.getPlayersItemFromCurrentEditable()
     return actualWeapon.materia.length < actualWeapon.materiaArity
   }
-, tryToRemoveMateriaFromItem() {
+, tryToRemoveMateriaFromItem: function() {
     if(!this.renderCurrentWeaponMateria.current) { return }
     this.renderCurrentWeaponMateria.removeCurrent()
-    this.player.weapons[this.currentWeapon].materia = this.renderCurrentWeaponMateria.array
+    this.player.weapons[this.editableItems.current].materia = this.renderCurrentWeaponMateria.array
     this.materias.array = this.player.computeUnusedMaterias()
   }
 , setActiveWeapon: function() {
-    this.weapon = Object.keys(this.player.weapons)[0]
-    this.currentWeapon = Object.keys(this.player.weapons)[0]
-    this.renderCurrentWeaponMateria = new Round(this.player.weapons[this.weapon].materia)
-    this.activeSprite = allSprites.get('weapon_'+this.weapon+'_large')
+    this.renderCurrentWeaponMateria = new Round(this.getPlayersItemFromCurrentEditable().materia)
+    this.activeSprite = allSprites.get('weapon_'+this.editableItems.current+'_large')
+    this.panels.array[0] = this.renderCurrentWeaponMateria
+  }
+, getPlayersItemFromCurrentEditable: function() {
+    return this.player.weapons[this.editableItems.current]
   }
 }
 
@@ -134,13 +173,35 @@ function arrangeMateriaPreviewText(materiaText, ctx)  {
   return _.merge(
     materiaText
   , floatWithin(
-      subRegion(padMenuBorder(paddedFullRegion(ctx)), [0,0.5,0.5,0.5])
-    , ['top', 'left']
+      shiftBoundsXY(padMenuBorder(subRegion(paddedFullRegion(ctx), [0,0.5,1,0.5])), 0, -2)
+    , ['top','left']
     , materiaText
     )
   )
 }
 
+function arrangeCurrentItemSlotsFilledText(text, ctx)  {
+  return _.merge(
+    text
+  , floatWithin(
+      currentWeaponMateriaRegion(ctx)
+    , ['top', 'right']
+    , text
+    )
+  )
+}
+
+function currentWeaponRegion(ctx) {
+  return padMenuBorder(subRegion(paddedFullRegion(ctx), [0,0,0.5,0.5]))
+}
+
+function currentWeaponMateriaRegion(ctx) {
+  return padMenuBorder(currentWeaponRegion(ctx))
+}
+
+function availableMateriaRegion(ctx) {
+  return padMenuBorder(subRegion(paddedFullRegion(ctx), [0.5,0,0.5,0.5]))
+}
 
 function arrangeActiveWeapon(activeWeapon, ctx)  {
   return _.merge(
@@ -166,6 +227,20 @@ function drawModalBackground(ctx) {
   ctx.fillRect(0, 0, ctx.width, ctx.height)
 }
 
+function generateMateriaListPreiewText(materiaDataName) {
+  var data = materiaData[materiaDataName]
+  return (data && data.type) || materiaDataName
+}
+
+function generateWorkbenchSynopsis(materiaDataName) {
+  var data = materiaData[materiaDataName]
+  if(!data) { return materiaDataName }
+  return [
+    data.name
+  , data.editor_description
+  ].join('\n')
+}
+
 function xywhObjHoldingSprite(sprite, x, y) {
   return {
     x: 0
@@ -175,4 +250,22 @@ function xywhObjHoldingSprite(sprite, x, y) {
   , sprite: sprite
   , draw: draw
   }
+}
+
+function shiftBoundsXY(bounds, shiftX, shiftY) {
+  return [
+    bounds[0] + shiftX
+  , bounds[1] + shiftY
+  , bounds[2]
+  , bounds[3]
+  ]
+}
+
+function drawHBar(ctx) {
+  var pfRegion = paddedFullRegion(ctx)
+  ctx.beginPath()
+  ctx.strokeStyle = colors.circuitMenuDivider
+  ctx.moveTo(pfRegion[0] + config.menuBorderSize,               pfRegion[1] + pfRegion[3]/2 + 0.5 + 2)
+  ctx.lineTo(pfRegion[0] + pfRegion[2] - config.menuBorderSize, pfRegion[1] + pfRegion[3]/2 + 0.5 + 2)
+  ctx.stroke()
 }
