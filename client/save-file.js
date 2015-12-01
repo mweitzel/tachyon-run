b9079451e26a8fd95cc742341c6f9582dabd6d6b
Mathew Weitzel <mweitzel@gmail.com>
2015-12-01 10:05:43 -0500

    static "save file", placeholder until saving logic

---
+++ client/save-file.js
@@ -0,0 +1,26 @@
+module.exports = { load: load }
+function load() {
+  return {
+    player: {
+      weapons: {
+        rod: { materia: [ 'redstone_bristar' ], materiaArity: 2 }
+      , fists: { materia: [ ], materiaArity: 4 }
+      , banana: { materia: [ ], materiaArity: 0 }
+      }
+    , materiaXP: {
+        // materia name    xp
+        redstone_bristar: 1
+      , bluestone_fortran: 1
+      , summon_beowulf_geonetti: 1
+      , bluestone_placehold_1: 1
+      , bluestone_placehold_2: 1
+      , bluestone_placehold_3: 1
+      , bluestone_placehold_4: 1
+      , bluestone_placehold_5: 1
+      , bluestone_placehold_6: 1
+      }
+    }
+  , lastSavePoint: null
+  , plotPoints: [ ]
+  }
+}
