16440c7460c637c80286101291b7a1ac35c6256b
Mathew Weitzel <mweitzel@gmail.com>
2015-12-01 10:47:39 -0500

    describe all materias here
    this is one heck of a design choice,
    this means there are limited materias,
    and you can only have one of each,
    that they come from specific places, or dont

---
+++ client/materias.js
@@ -0,0 +1,24 @@
+module.exports = {
+  redstone_bristar: {
+    type: 'fire'
+  , sprite_prefix: ''
+  , name: "Brinstar's Heartflake"
+  }
+, bluestone_fortran: {
+    type: 'ice'
+  , sprite_prefix: ''
+  , name: 'Fortran Blue'
+  }
+, summon_beowulf_geonetti: {
+    type: 'summon'
+  , sprite_prefix: ''
+  , name: 'Beowulf Geonetti'
+  , editor_description: [
+      'Cluster of syncronous autos.'
+    , ''
+    , '"open skies tare to thunder"'
+    ].join('\n')
+  }
+}
+//    '123456789012345678901234567890123456789012' largest string description length from menu width
+//    4 lines tall
