130e0e26c766131863d8d7ce0941c3880887c299
Mathew Weitzel <mweitzel@gmail.com>
2015-12-05 11:11:17 -0500

    add comment to oscillate module, provide meaning

--- oscillate-between-values.js
+++ oscillate-between-values.js
@@ -2,6 +2,10 @@
 x=0  ; f(x)=a
 x=pi ; f(x)=b
 ((a-b)/2) * cos(x) + ((a+b)/2)
+
+cos oscilates from 1 to -1
+use this to oscilate between two provided values
+
 */
 
 module.exports = function(a, b, x) {
