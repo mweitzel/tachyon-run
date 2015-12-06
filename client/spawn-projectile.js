0f8a8ca3c4f6ca5923e6d68b7e0fbfdb79b6bb6a
Mathew Weitzel <mweitzel@gmail.com>
2015-12-05 21:53:57 -0500

    particles drift in direction you tell them to

--- client/spawn-projectile.js
+++ client/spawn-projectile.js
@@ -104,7 +104,14 @@ function Particle(spriteName, options) {
   this.sprite = sprites.get(spriteName) || sprites.get('not_found')
   this.x = options.x
   this.y = options.y
-  this.spriteX = this.x - this.sprite.width/2
-  this.spriteY = this.y - this.sprite.height/2
 }
-Particle.prototype.draw = Sprite.draw
+
+Particle.prototype = {
+  update: function(core) {
+    this.x += this.dx
+    this.y += this.dy
+  }
+, draw: Sprite.draw
+, get spriteX() { this.x - this.sprite.width/2 }
+, get spriteY() { this.y - this.sprite.height/2 }
+}
