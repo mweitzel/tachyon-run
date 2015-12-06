1282dd7fcabe6f11b37faa3b464b0c9e2e8291ca
Mathew Weitzel <mweitzel@gmail.com>
2015-12-05 22:57:16 -0500

    fix 0f8a8ca, that was sloppy

--- client/particle.js
+++ client/particle.js
@@ -4,17 +4,19 @@ var Sprite = require('./sprite')
 module.exports = Particle
 
 function Particle(spriteName, options) {
-  this.sprite = sprites.get(spriteName) || sprites.get('not_found')
+  this.sprite = sprites.get(spriteName)
   this.x = options.x
   this.y = options.y
 }
 
 Particle.prototype = {
-  update: function(core) {
+  dx: 0
+, dy: 0
+, update: function(core) {
     this.x += this.dx
     this.y += this.dy
   }
 , draw: Sprite.draw
-, get spriteX() { this.x - this.sprite.width/2 }
-, get spriteY() { this.y - this.sprite.height/2 }
+, get spriteX() { return this.x - this.sprite.width/2 }
+, get spriteY() { return this.y - this.sprite.height/2 }
 }
