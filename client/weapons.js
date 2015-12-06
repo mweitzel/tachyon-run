74ce4ab47969b46483bab2f20e5dc0a910485ce9
Mathew Weitzel <mweitzel@gmail.com>
2015-12-05 21:22:59 -0500

    add placeholder "wrench" weapon data

--- client/weapons.js
+++ client/weapons.js
@@ -8,4 +8,13 @@ module.exports = {
   , lifeInMs: 300
   , velocity: 0.2
   }
+, wrench: {
+    baseDamage: 10
+  , projectileSprite: 'dummy'
+  , contrailSprite: 'charlie'
+  , emitContrailPeriod: 30
+  , firePeriod: 200
+  , lifeInMs: 300
+  , velocity: 0.2
+  }
 }
