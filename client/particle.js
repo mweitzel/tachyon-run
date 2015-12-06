c9acb4d9208156000068d3eb2fc881e4beb366f4
Mathew Weitzel <mweitzel@gmail.com>
2015-12-05 21:58:34 -0500

    extract Particle from spawn-projectile

---
+++ client/particle.js
@@ -0,0 +1,18 @@
+var Sprite = require('./sprite')
+module.exports = Particle
+
+function Particle(spriteName, options) {
+  this.sprite = sprites.get(spriteName) || sprites.get('not_found')
+  this.x = options.x
+  this.y = options.y
+}
+
+Particle.prototype = {
+  update: function(core) {
+    this.x += this.dx
+    this.y += this.dy
+  }
+, draw: Sprite.draw
+, get spriteX() { this.x - this.sprite.width/2 }
+, get spriteY() { this.y - this.sprite.height/2 }
+}
--- client/spawn-projectile.js
+++ client/spawn-projectile.js
@@ -1,13 +1,14 @@
 var removeAfter = require('./remove-obj-after-time')
   , DynamicCollider = require('./dynamic-collider')
-  , sprites = require('./all-sprites')
   , Sprite = require('./sprite')
+  , sprites = require('./all-sprites')
   , _ = require('lodash')
   , delay = 200
   , zIndex = require('./layer-z-defaults').projectile
   , beget = require('../beget')
   , timedRemove = require('./remove-obj-after-time')
   , config = require('./config')
+  , Particle = require('./particle')
 
 module.exports = spawnProjectile
 
@@ -98,20 +99,4 @@ Projectile.prototype.randomPoint = function(scaler) {
     x: this.x + (Math.random() - 0.5) * scaler
   , y: this.y + (Math.random() - 0.5) * scaler
   }
-}
-
-function Particle(spriteName, options) {
-  this.sprite = sprites.get(spriteName) || sprites.get('not_found')
-  this.x = options.x
-  this.y = options.y
-}
-
-Particle.prototype = {
-  update: function(core) {
-    this.x += this.dx
-    this.y += this.dy
-  }
-, draw: Sprite.draw
-, get spriteX() { this.x - this.sprite.width/2 }
-, get spriteY() { this.y - this.sprite.height/2 }
 }
