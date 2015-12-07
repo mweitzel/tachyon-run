mweitzel/tachyon-run

commit 29563f10e9076ee9aef23dcaf08561f56fab1415

yes-no to use float method

client/yes-no.js
@@ -15,17 +15,6 @@ function YesNo(core, questionText, successCallback, yesText, noText) {
     this.exit()
   }
 
-  var menu = new BaseMenu(core, menuAttrs, menuOptions)
-
-  _.merge(
-    menu
-  , floatWithin(
-      pad(
-        [ 0, 0, core.context.width, core.context.height ]
-      , config.tileSize
-      )
-    , ['middle', 'right']
-    , menu.rstring
-    )
-  )
+  ( new BaseMenu(core, menuAttrs, menuOptions)
+  ).float(['middle', 'right'])
 }

commit 1c58ab5efc092f55e3fd8d235224a93a6c2a0841

fix bug introduced in 29563f1
anonymous function unintentionally invoked on instanciation

client/yes-no.js
@@ -15,6 +15,6 @@ function YesNo(core, questionText, successCallback, yesText, noText) {
     this.exit()
   }
 
-  ( new BaseMenu(core, menuAttrs, menuOptions)
+  ;( new BaseMenu(core, menuAttrs, menuOptions)
   ).float(['middle', 'right'])
 }