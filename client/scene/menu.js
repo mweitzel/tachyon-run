3bc2a5a8d1806911d2f3896619c443057977b9dc
Mathew Weitzel <mweitzel@gmail.com>
2015-12-07 10:48:11 -0500

    update menu to use new libraries

--- client/scene/menu.js
+++ client/scene/menu.js
@@ -1,42 +1,17 @@
-var keys = require('../keys')
-  , atlas = require('../atlas')
-  , Sprite = require('../sprite-preconfigured')
+var Menu = require('../base-menu')
+  , follow = require('../follow')
+  , boundHelper = require('../bounds-helper')
 
 module.exports = function(core) {
-  core.entities.push(new MenuObject())
-  core.entities.push(new Dude(10, 10))
-  core.entities.push(new Dude(50, 50))
-  core.entities.push(new Dude(-50, -50))
-  core.entities.push(new Dude(-50, 50))
-  core.entities.push(new Dude(50, -50))
+  var menu = new Menu(
+    core
+  , { border: true, isBackgroundObject: true }
+  , {
+      'new game': noop
+    , continue: noop
+    , settings: noop
+    }
+  ).float(['middle'])
 }
 
-function MenuObject() { }
-
-MenuObject.prototype = {
-  update: function(core) {
-    if(core.input.getKeyDown(keys.ENTER))
-      core.sceneLoader.load('level-editor')
-  }
-}
-
-function Dude(x, y) {
-  this.x = x
-  this.y = y
-  this.sprite = new Sprite('charles')
-}
-
-Dude.prototype = {
-  x: 0
-, y: 0
-, update: function() {
-    if(Math.random() < 0.9) { return }
-    Math.random() > 0.5
-      ? this.x++
-      : this.x--
-    Math.random() > 0.5
-      ? this.y++
-      : this.y--
-  }
-, draw: Sprite.draw
-}
+function noop() {}
