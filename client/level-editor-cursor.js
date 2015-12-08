0e1ab0449067609c742490f30b39118e13e08c62
Mathew Weitzel <mweitzel@gmail.com>
2015-12-08 14:42:46 -0500

    add HJKL like vim arrows for editor navigation

--- client/level-editor-cursor.js
+++ client/level-editor-cursor.js
@@ -16,16 +16,16 @@ Cursor.prototype = {
 , get z() { return zLayers.gui }
 , update: function(core) {
     var pressed = core.input.getKeyDown.bind(core.input)
-    if(pressed(keys.RIGHT)) { this.x += 16 }
-    if(pressed(keys.LEFT))  { this.x -= 16 }
-    if(pressed(keys.UP))    { this.y -= 16 }
-    if(pressed(keys.DOWN))  { this.y += 16 }
+    if(pressed(keys.RIGHT) || pressed(keys.L)) { this.x += 16 }
+    if(pressed(keys.LEFT)  || pressed(keys.H))  { this.x -= 16 }
+    if(pressed(keys.UP)    || pressed(keys.K))    { this.y -= 16 }
+    if(pressed(keys.DOWN)  || pressed(keys.J))  { this.y += 16 }
     var isDown = core.input.getKey.bind(core.input)
     if(isDown(keys.SHIFT)) {
-      if(isDown(keys.RIGHT)) { this.x += 16 }
-      if(isDown(keys.LEFT))  { this.x -= 16 }
-      if(isDown(keys.UP))    { this.y -= 16 }
-      if(isDown(keys.DOWN))  { this.y += 16 }
+      if(isDown(keys.RIGHT) || isDown(keys.L)) { this.x += 16 }
+      if(isDown(keys.LEFT)  || isDown(keys.H))  { this.x -= 16 }
+      if(isDown(keys.UP)    || isDown(keys.K))    { this.y -= 16 }
+      if(isDown(keys.DOWN)  || isDown(keys.J))  { this.y += 16 }
     }
   }
 , sprite: new Sprite('highlight')
