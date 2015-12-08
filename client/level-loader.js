d7665a8873694fb7cd5e66d924cbc4ea70c5773d
Mathew Weitzel <mweitzel@gmail.com>
2015-12-08 11:55:31 -0500

    let specified pieces persist through level-change

--- client/level-loader.js
+++ client/level-loader.js
@@ -1,6 +1,13 @@
+var _ = require('lodash')
+
 module.exports = function(core, recursiveSerializedToGOFn, levelName, cb) {
+  var persisted = _.filter(core.entities, { persistLevelLoad: true })
   core.sceneLoader.load('blank', function() {
     recursiveSerializedToGOFn(levelName).forEach(function(levelPiece) {
+      core.entities.push(levelPiece)
+    })
+    persisted.forEach(function(levelPiece) {
+      levelPiece.onLevelChange && levelPiece.onLevelChange(core)
       core.entities.push(levelPiece)
     })
     cb && cb(core)
