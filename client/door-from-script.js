bfa768027aa424efc8200ac443d7f7fec4c030dd
Mathew Weitzel <mweitzel@gmail.com>
2015-12-08 10:29:06 -0500

    doors are properly usable

--- client/door-from-script.js
+++ client/door-from-script.js
@@ -1,18 +1,21 @@
 var config = require('./config')
   , StaticCollider = require('./static-collider')
+  , levelLoader = require('./level-loader')
+  , setupSmoothCamera = require('./set-up-smooth-camera-for-loaded-level-and-player')
 
 module.exports = makeDoorFromScript
 
-function makeDoorFromScript(levelName, xString, yString) {
+function makeDoorFromScript(recursiveSerializedToGOFn, levelName, xString, yString) {
   var targetX = parseInt(xString)
   var targetY = parseInt(yString)
-  return new Door(this.x, this.y, levelName, targetX, targetY)
+  return new Door(this.x, this.y, recursiveSerializedToGOFn, levelName, targetX, targetY)
 }
 
 
-function Door(x, y, levelName, targetX, targetY) {
+function Door(x, y, recursiveSerializedToGOFn, levelName, targetX, targetY) {
   this.x = x
   this.y = y
+  this.recursiveSerializedToGOFn = recursiveSerializedToGOFn
   this.levelName = levelName
   this.targetX = targetX
   this.targetY = targetY
@@ -23,12 +26,17 @@ Door.prototype = {
 , bounds: function() { return [this.x, this.y, this.width, this.height] }
 , width: config.tileSize
 , height: config.tileSize
-, use: enterDoor
+, use: function(player, core) { enterDoor.call(this, this.recursiveSerializedToGOFn, player, core) }
 , drawDebug: StaticCollider.prototype.drawDebug
 , squidgedBoundsForDebugDraw: StaticCollider.prototype.squidgedBoundsForDebugDraw
 }
 
-function enterDoor(user, core) {
-  console.log('going to level '+this.levelName)
-  console.log('coords '+this.targetX+','+this.targetY)
+function enterDoor(recursiveSerializedToGOFn, player, core) {
+  levelLoader(core, recursiveSerializedToGOFn, this.levelName, function(core) {
+    player.x = this.targetX + config.tileSize/2
+    player.y = this.targetY + config.tileSize/2
+    core.entities.push(player)
+    setupSmoothCamera(core, player)
+  }.bind(this)
+  )
 }
