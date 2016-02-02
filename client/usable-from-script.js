04c9af0244a79df718387808d3d0e150434b1861
Mathew Weitzel <mweitzel@gmail.com>
2016-02-02 12:48:23 -0500

    bind editor arguments to usable objects

--- client/usable-from-script.js
+++ client/usable-from-script.js
@@ -5,6 +5,7 @@ var _ = require('lodash')
 
 var usable = addUsableComponentsToLevelEditorPiece.usable = {
   bench: require('./usable/bench')
+, reader: require('./usable/reader')
 }
 
 // assumes 'this' will be the level editor script object
@@ -12,7 +13,13 @@ module.exports = addUsableComponentsToLevelEditorPiece
 
 function addUsableComponentsToLevelEditorPiece() {
   var usableIdentifier = arguments[0]
-  this.use = usable[usableIdentifier]
+  this.use = usable[usableIdentifier].bind(this)
+  var remainingArgs = Array.prototype.slice.call(arguments, 1)
+  while(remainingArgs[0]) {
+    var currentArg = remainingArgs[0]
+    this.use = this.use.bind(this, currentArg)
+    remainingArgs = remainingArgs.slice(1)
+  }
   delegate(this, this.sprite, 'width')
   delegate(this, this.sprite, 'height')
   _.merge(this, StaticCollider.prototype)
