47c8860ec2139b291a9e86169f77b60c29be9d51
Mathew Weitzel <mweitzel@gmail.com>
2016-02-06 13:08:38 -0500

    media loader now has sound-atlas and track map

--- client/media-loader.js
+++ client/media-loader.js
@@ -3,6 +3,7 @@ var fs = require('fs')
 module.exports = {
   sounds: {
     thunk: [ 'mp3', fs.readFileSync('test/media/thunk.mp3', 'base64') ]
+  , atlas: [ 'ogg', fs.readFileSync('media/sound-atlas.ogg', 'base64') ]
   }
 , images: {
     atlas: [ 'png', fs.readFileSync('media/atlas-1.png', 'base64') ]
@@ -11,5 +12,6 @@ module.exports = {
 , text: {
     atlas: [ 'json', fs.readFileSync('media/atlas-1.json', 'ascii') ]
   , sprite: [ 'json', fs.readFileSync('media/sprite-meta.json', 'ascii') ]
+  , sounds: [ 'json', fs.readFileSync('media/sound-atlas.json', 'ascii') ]
   }
 }
