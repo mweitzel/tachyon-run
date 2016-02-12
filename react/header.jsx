efc921f9872a7e708a39d466538bbe7c0276446c
Mathew Weitzel <mweitzel@gmail.com>
2016-02-12 09:52:27 -0500

    add header components

---
+++ react/header.jsx
@@ -0,0 +1,16 @@
+var React = require('react')
+
+module.exports = React.createClass({
+  render: function() {
+    return (
+      <div className='header-holder'>
+        <div className='tile' />
+        <div className='left' />
+        <div className='words'>
+          <h1> Tachyon Run </h1>
+          <h2> Sign up for Alpha Testing </h2>
+        </div>
+      </div>
+    )
+  }
+})
