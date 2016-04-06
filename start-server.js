955b662f83741fad490f8048f37c0616e2a9e4b1
Mathew Weitzel <mweitzel@gmail.com>
2016-04-06 12:31:13 -0400

    don't fetch env HOST because heroku doesn't use it

--- start-server.js
+++ start-server.js
@@ -1,9 +1,10 @@
 var initTime = new Date().getTime()
   , createServer = require('./server')
   , fetch = require('./fetch-env')
-  , PORT=fetch('PORT')
+  , PORT = fetch('PORT')
+  , host = porcess.env.HOST || 'production'
 
 createServer.listen(fetch('PORT'))
 
-console.log("Server running at http://"+fetch('HOST')+":"+fetch('PORT')+"/")
+console.log("Server running at http://"+host+":"+fetch('PORT')+"/")
 console.log('started server in', new Date().getTime() - initTime, 'ms')
