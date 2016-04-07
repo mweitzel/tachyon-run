51e902445163097ae1d371a194dacd50e86d60ee
Mathew Weitzel <mweitzel@gmail.com>
2016-04-07 13:22:26 -0400

    add server logging

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 var koa = require('koa')
+  , morgan = require('koa-morgan')
   , routes = require('./routes')
 
 var app = module.exports = koa()
 
-var middleware = routes
+var middleware = [
+  morgan.middleware('combined', {})
+].concat(routes)
 
 for(var i in middleware)
   app.use(middleware[i])
